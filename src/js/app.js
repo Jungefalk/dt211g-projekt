47cb6b1c7e5fc038e6e11fa931af31d708967ffc
Caroline <c.jungefalk@gmail.com>
2025-03-19 12:39:38 +0100

    eventlistener and function added for window on load

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 "use strict"
 
-//get elements by id and store them in varaible
+//get elements by id and store them in variable
 
 /** @type {HTMLDivElement} Element where geolocated region loads */
 const geoRegionEl = document.getElementById("geoRegion");
@@ -10,3 +10,14 @@ const allRegionsEl = document.getElementById("allRegions");
 
 /** @type {HTMLDivElement} Element where the bar chart is loaded*/
 const barChartEl = document.getElementById("barChart");
+
+
+//Eventlistener 
+window.addEventListener("load", init);
+
+
+function init(){
+
+    console.log("Sidan har laddat in")
+    fetchUserLocation()
+}
