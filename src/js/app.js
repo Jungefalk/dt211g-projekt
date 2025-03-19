eed3b3b126e1c3d05f4db4096a8376163932f220
Caroline <c.jungefalk@gmail.com>
2025-03-19 12:17:51 +0100

    got the elements by id and stored in variables

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+"use strict"
+
+//get elements by id and store them in varaible
+
+/** @type {HTMLDivElement} Element where geolocated region loads */
+const geoRegionEl = document.getElementById("geoRegion");
+
+/** @type {HTMLDivElement} Element where all regions where there are measuring stations are loaded*/
+const allRegionsEl = document.getElementById("allRegions");
+
+/** @type {HTMLDivElement} Element where the bar chart is loaded*/
+const barChartEl = document.getElementById("barChart");
